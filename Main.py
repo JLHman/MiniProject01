a45e3b978b736d1ff8b3893d824fb75ee941b798
Jesse Heckman <jlheckman@gmail.com>
2022-09-17 10:26:10 -0500

    This is the first commit, updated the readme file with project title

---
+++ Main.py
@@ -0,0 +1,26 @@
+# INF601 - Advanced Programming in Python
+# Jesse Heckman
+# Mini Project 1
+
+
+import yfinance as yf
+
+#rok = yf.Ticker("ROK")
+
+# get stock info
+#rok.info
+
+# get historical market data
+#hist = rok.history(period="max")
+
+#tickers = yf.Tickers('rok csco abb sbgsy ge')
+
+data = yf.download("ROK", start="2022-09-01", end="2022-09-16")
+#print(data)
+
+rokPrices = []
+
+for price in data['Adj Close']:
+    rokPrices.append(price)
+
+print(rokPrices)
