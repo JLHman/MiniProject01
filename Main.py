28e768dd1f8cf9d17b6cbc031b2604fa0e796ae9
Jesse Heckman <jlheckman@gmail.com>
2022-09-17 15:28:25 -0500

    Add five of the stocks to the charts file.  Look at the closing prices for the last 10 days.

--- Main.py
+++ Main.py
@@ -6,22 +6,13 @@ import numpy as np
 import yfinance as yf
 import matplotlib.pyplot as plt
 
-
-
-
-# rok = yf.Ticker("ROK")
-
-# get stock info
-# rok.info
-
-# get historical market data
-# hist = rok.history(period="max")
-
+# get data for Rockwell stock from yfinance api
 data = yf.download("ROK", start="2022-09-01", end="2022-09-16")
 # print(data)
 
 rokPrices = []
 
+# Get the closing price from the data output
 for price in data['Adj Close']:
     rokPrices.append(price)
 
@@ -40,6 +31,103 @@ plt.savefig('charts/rok.png')
 # Show Graph
 plt.show()
 
+# get data for Cisco stock from yfinance api
+data = yf.download("CSCO", start="2022-09-01", end="2022-09-16")
+# print(data)
 
+cscoPrices = []
 
+# Get the closing price from the data output
+for price in data['Adj Close']:
+    cscoPrices.append(price)
+
+print(cscoPrices)
+
+# Create a numpy array
+cscoarray = np.array(cscoPrices)
+# Create a Graph with matplotlib and label y axis
+plt.plot(cscoarray)
+plt.ylabel('Stock Price')
+plt.xlabel('Days')
+
+# Save Graph
+plt.savefig('charts/csco.png')
+
+# Show Graph
+plt.show()
+
+# get data for ABB stock from yfinance api
+data = yf.download("ABB", start="2022-09-01", end="2022-09-16")
+# print(data)
+
+abbPrices = []
+
+# Get the closing price from the data output
+for price in data['Adj Close']:
+    abbPrices.append(price)
+
+print(abbPrices)
+
+# Create a numpy array
+abbarray = np.array(abbPrices)
+# Create a Graph with matplotlib and label y axis
+plt.plot(abbarray)
+plt.ylabel('Stock Price')
+plt.xlabel('Days')
+
+# Save Graph
+plt.savefig('charts/abb.png')
+
+# Show Graph
+plt.show()
+
+# get data for GE stock from yfinance api
+data = yf.download("GE", start="2022-09-01", end="2022-09-16")
+# print(data)
+
+gePrices = []
+
+# Get the closing price from the data output
+for price in data['Adj Close']:
+    gePrices.append(price)
+
+print(gePrices)
+
+# Create a numpy array
+gearray = np.array(gePrices)
+# Create a Graph with matplotlib and label y axis
+plt.plot(gearray)
+plt.ylabel('Stock Price')
+plt.xlabel('Days')
+
+# Save Graph
+plt.savefig('charts/ge.png')
+
+# Show Graph
+plt.show()
+
+# get data for Schneider Electric stock from yfinance api
+data = yf.download("SBGSY", start="2022-09-01", end="2022-09-16")
+# print(data)
+
+sbgsyPrices = []
+
+# Get the closing price from the data output
+for price in data['Adj Close']:
+    sbgsyPrices.append(price)
+
+print(sbgsyPrices)
+
+# Create a numpy array
+sbgsyarray = np.array(sbgsyPrices)
+# Create a Graph with matplotlib and label y axis
+plt.plot(sbgsyarray)
+plt.ylabel('Stock Price')
+plt.xlabel('Days')
+
+# Save Graph
+plt.savefig('charts/sbgsy.png')
+
+# Show Graph
+plt.show()
 
